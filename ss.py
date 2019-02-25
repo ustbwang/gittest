1e7eb4fa344af7ec73830ee77fdfb74de3ad55a1
unknown <wangchao9@corp.qihoo.net>
2019-02-25 17:36:30 +0800

    this is test

---
+++ ss.py
@@ -0,0 +1,7 @@
+import pandas as pd
+columns = ['idea_id', 'pv', 'title', 'pro-id', 'idea_url', 'url']
+data = pd.read_csv('extra_ES_candidate.txt', header=None, sep='\t', names=columns)
+
+data = data[['idea_id', 'pv', 'idea_url', 'url', 'title']]
+
+data.to_csv('qnm.txt', index=False, header=None, sep='\t')
