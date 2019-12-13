7049d018892e2dc17174d1a7076ced0c7f27d731
xx <xx@xxdeMacBook-Pro.local>
2019-12-13 11:30:39 +0800

    ss.py

--- ss.py
+++ ss.py
@@ -2,9 +2,10 @@ import pandas as pd
 columns = ['idea_id', 'pv', 'title', 'pro-id', 'idea_url', 'url']
 data = pd.read_csv('extra_ES_candidate.txt', header=None, sep='\t', names=columns)
 
-data = data[['idea_id', 'pv', 'idea_url', 'url', 'title']]
-
-data.to_csv('qnm.txt', index=False, header=None, sep='\t')
+#data = data[['idea_id', 'pv', 'idea_url', 'url', 'title']]
+#data = data[['idea_id', 'pv', 'idea_url', 'url', 'title']]
+data = data[["'idea_id', 'pv'"]]
+#data.to_csv('qnm.txt', index=False, header=None, sep='\t')
 print("dev branch test 3!")
 print("qnm")
 print("qnmmm")
